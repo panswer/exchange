81cbd88750be013cb3fd9264ab16d4731b710fbe
ricardo mejias <panswer.rjmm@gmail.com>
2022-11-10 16:04:24 -0400

    create mocks on middleware, services, logger and make a folder with response
    create mocks on lambda function signIn to middleware, services and utils logger

--- tests/controlles/signIn.test.ts
+++ tests/controlles/signIn.test.ts
@@ -1,20 +1,45 @@
-import { doRequest } from "../../utils/handlerCall";
+import { httpRequestData } from "../helpers/interfaces/httpRequest";
+import { doRequest } from "../helpers/handlerRequest";
 
-import { signInSuccess } from "../mocks/signIn";
+import { signInSuccess } from "../mocks/controllers/signInMocks";
 
-const functionName = 'signIn';
+const functionName = "signIn";
 
-const mockSignInFlow = jest.fn(async (email, password) => {
-  if (!email || !password) {
-    throw new Error("Error test");
-  } else {
-    return signInSuccess;
-  }
-});
+const mockSignInFlow = jest.fn().mockResolvedValue(signInSuccess);
+
+const mockHttpJsonBodyParser = jest.fn();
+
+const mockCatchErrorOnError = jest.fn(() => ({
+  statusCode: 500,
+}));
+
+const mockValidator = jest.fn();
+
+const mockWriteLogger = jest.fn();
 
 jest.mock("../../src/services/CognitoService", () => ({
   getInstance: () => ({
     getSessionToken: mockSignInFlow,
+  }),
+}));
+
+jest.mock("@middy/http-json-body-parser", () => () => ({
+  before: mockHttpJsonBodyParser,
+}));
+
+jest.mock("../../src/middlewares/CatchError", () => ({
+  CatchError: {
+    onError: mockCatchErrorOnError,
+  },
+}));
+
+jest.mock("@middy/validator", () => () => ({
+  before: mockValidator,
+}));
+
+jest.mock("../../src/utils/Logger", () => ({
+  getInstance: () => ({
+    writeLogger: mockWriteLogger,
   }),
 }));
 
@@ -25,9 +50,11 @@ describe("Auth - Sign in", () => {
       password: "Ricardo.1",
     };
 
-    const result = await doRequest(functionName, {
+    const requestData: httpRequestData = {
       body,
-    });
+    };
+
+    const result = await doRequest(functionName, requestData);
 
     expect(result.statusCode).toBe(202);
 
@@ -37,13 +64,17 @@ describe("Auth - Sign in", () => {
   });
 
   test("Catch data without password", async () => {
+    mockValidator.mockImplementationOnce(() => {
+      throw new Error("Test error");
+    });
     const body = {
       email: "ricardo@mftech.io",
     };
-
-    const result = await doRequest(functionName, {
+    const requestData: httpRequestData = {
       body,
-    });
+    };
+
+    const result = await doRequest(functionName, requestData);
 
     expect(result.statusCode).toBe(500);
   });
