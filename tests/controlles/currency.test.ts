8a7f979dbf59d889716b230b2792e22fac9d2ccd
ricardo mejias <panswer.rjmm@gmail.com>
2022-11-10 15:51:42 -0400

    create mock on middlewares, services and logger
    mock on currency controller to middlewares, services and loggers

--- tests/controlles/currency.test.ts
+++ tests/controlles/currency.test.ts
@@ -1,70 +1,36 @@
-import { doRequest } from "../../utils/handlerCall";
-import { CurrencyRequestInterface } from "../../src/interfaces/CurrencyRequestInterface";
-import { ApiLayerResponseInterface } from "../../src/interfaces/ApiLayerResponseInterface";
-import { CurrencyRequest } from "../../src/interfaces/DynamodbService";
+import { httpRequestData } from "../helpers/interfaces/httpRequest";
+import { doRequest } from "../helpers/handlerRequest";
+
+import {
+  currencyRequestSucessMock,
+  getExchangeCurrencyResponseSuccessMock,
+  saveCurrencyRequestSuccessMock,
+  currencyRequestBadMock,
+} from "../mocks/controllers/currencyMocks";
+
+import { requestContextSuccessMock } from "../mocks/generic/request";
 
 const functionName = "currency";
 
-const mockGetExchangeCurrency = jest.fn(
-  (data: CurrencyRequestInterface): Promise<ApiLayerResponseInterface> =>
-    new Promise((resolve, reject) => {
-      if (
-        (!data.amount && typeof data.amount !== "number") ||
-        !data.from ||
-        !data.to
-      ) {
-        reject("Test error with request");
-      } else {
-        resolve({
-          date: "",
-          info: {
-            rate: 1,
-            timestamp: new Date().getMilliseconds(),
-          },
-          query: {
-            amount: data.amount,
-            from: data.from,
-            to: data.to,
-          },
-          result: data.amount,
-          success: true,
-        });
-      }
-    })
-);
+const mockGetExchangeCurrency = jest
+  .fn()
+  .mockResolvedValue(getExchangeCurrencyResponseSuccessMock);
 
-/* 
+const mockSaveCurrencyRequest = jest.fn();
 
-    currencyTo: string,
-    currencyFrom: string,
-    amount: number,
-    createdAt: Date = new Date(),
-    amountResult: number,
-    username: string
+const mockHttpJsonBodyParser = jest.fn();
 
-*/
-const mockSaveCurrencyRequest = jest.fn(
-  (params: CurrencyRequest) =>
-    new Promise((resolve, reject) => {
-      if (
-        !params.currencyTo ||
-        !params.currencyFrom ||
-        (!params.amount && typeof params.amount !== "number") ||
-        !params.amountResult ||
-        !params.username
-      ) {
-        console.log("#".repeat(50));
-        console.log(params);
-        console.log("#".repeat(50));
+const mockCatchErrorOnError = jest.fn(() => ({
+  statusCode: 500,
+}));
 
-        reject(new Error("Test error without any data"));
-      } else {
-        resolve({});
-      }
-    })
-);
+const mockLogUserBefore = jest.fn();
 
-process.env.TEST_ON = "1";
+const mockValidatorBefore = jest.fn();
+
+const mockCurrencyMiddlewareBefore = jest.fn();
+
+const mockWriteLogger = jest.fn();
 
 jest.mock("../../src/services/CurrencyApiService", () => ({
   getInstance: () => ({
@@ -78,58 +44,89 @@ jest.mock("../../src/models/CurrencyRequestModel", () => ({
   }),
 }));
 
+jest.mock("@middy/http-json-body-parser", () => () => ({
+  before: mockHttpJsonBodyParser,
+}));
+
+jest.mock("../../src/middlewares/CatchError", () => ({
+  onError: mockCatchErrorOnError,
+}));
+
+jest.mock("@middy/validator", () => () => ({
+  before: mockValidatorBefore,
+}));
+
+jest.mock("../../src/middlewares/LogUser", () => ({
+  LogUser: {
+    before: mockLogUserBefore,
+  },
+}));
+
+jest.mock("../../src/middlewares/CurrencyMiddleware", () => ({
+  CurrencyMiddleware: {
+    before: mockCurrencyMiddlewareBefore,
+  },
+}));
+
+jest.mock("../../src/utils/Logger", () => ({
+  getInstance: () => ({
+    writeLogger: mockWriteLogger,
+  }),
+}));
+
 describe("Exchange - Basic", () => {
   test("Get exchange rate success", async () => {
-    const body = {
-      from: "USD",
-      to: "EUR",
-      amount: 2,
-    };
-
-    let result = await doRequest(functionName, {
-      body,
-      requestContext: {
-        authorizer: {
-          claims: { username: "test@mftech.io" },
-        },
-      },
-    });
-
-    expect(result.statusCode).toBe(200);
-    // await expect(
-    //   doRequest(functionName, {
-    //     body,
-    //     requestContext: {
-    //       authorizer: { claims: { username: "test@mftech.io" } },
-    //     },
-    //   })
-    // ).resolves.toHaveProperty("statusCode", 200);
-  });
-
-  test("Request without currency from", async () => {
-    const body = {
-      to: "EUR",
-      amount: 2,
-    };
-
-    const result = await doRequest(functionName, { body });
-
-    expect(result.statusCode).toBe(500);
-  });
-
-  test("Good Request but don't save", async () => {
-    const body = {
-      to: "EUR",
-      from: "VES",
-      amount: 3,
-    };
-
-    mockSaveCurrencyRequest.mockRejectedValueOnce(
-      () => new Error("Test in save")
+    mockSaveCurrencyRequest.mockImplementationOnce(
+      () => saveCurrencyRequestSuccessMock
     );
 
-    const result = await doRequest(functionName, { body });
+    const body = currencyRequestSucessMock;
 
-    expect(result.statusCode).toBe(500);
+    const requestData: httpRequestData = {
+      body,
+      requestContext: requestContextSuccessMock,
+    };
+
+    let result = await doRequest(functionName, requestData);
+
+    expect(result.statusCode).toBe(200);
+
+    const resultJSON = JSON.parse(result.body).amounts;
+
+    expect(resultJSON.query.amount).toBe(currencyRequestSucessMock.amount);
+    expect(resultJSON.query.from).toBe(currencyRequestSucessMock.from);
+    expect(resultJSON.query.to).toBe(currencyRequestSucessMock.to);
+  });
+
+  test("Request without currency from", () => {
+    mockGetExchangeCurrency.mockRejectedValueOnce(new Error("Test Error"));
+
+    const body = currencyRequestBadMock;
+
+    const requestData: httpRequestData = {
+      body,
+      requestContext: requestContextSuccessMock,
+    };
+
+    expect(doRequest(functionName, requestData)).resolves.toHaveProperty(
+      "statusCode",
+      500
+    );
+  });
+
+  test("Good Request but don't save", () => {
+    mockSaveCurrencyRequest.mockRejectedValueOnce(new Error("Test error"));
+
+    const body = currencyRequestSucessMock;
+
+    const requestData: httpRequestData = {
+      body,
+      requestContext: requestContextSuccessMock,
+    };
+
+    expect(doRequest(functionName, requestData)).resolves.toHaveProperty(
+      "statusCode",
+      500
+    );
   });
 });
---
+++ tests/mocks/generic/request.ts
@@ -0,0 +1,5 @@
+export const requestContextSuccessMock = {
+  authorizer: {
+    claims: { username: "test@mftech.io" },
+  },
+};
