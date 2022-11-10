f4bc088c3524c4cb555d689448f3fc3e44629f9b
ricardo mejias <panswer.rjmm@gmail.com>
2022-11-10 16:17:54 -0400

    mock to services
    mock on CurrencyApiService to axios request with mock implementation

--- tests/services/CurrencyApiService.test.ts
+++ tests/services/CurrencyApiService.test.ts
@@ -3,53 +3,67 @@ import CurrencyApiService from "../../src/services/CurrencyApiService";
 process.env.API_LAYER_KEY = "123";
 process.env.API_LAYER_URL = "123";
 
-// const mockGetFn = jest.fn(() => ({
-//   data: {},
-// }));
+const mockGetFn = jest.fn().mockResolvedValue({
+  data: {},
+});
 
 jest.mock("axios", () => ({
-  get: () => ({
-    data: {},
-  }),
+  get: () => mockGetFn(),
 }));
 
 describe("Api Layer", () => {
-  beforeEach(() => {
-    CurrencyApiService.destroyInstance();
-  });
+  describe("Test about instance of class", () => {
+    test("Get an instance", () => {
+      const currencyApiService = CurrencyApiService.getInstance();
 
-  test("Get an instance", () => {
-    const currencyApiService = CurrencyApiService.getInstance();
-
-    expect(currencyApiService).toBeInstanceOf(CurrencyApiService);
-  });
-
-  test("Get twice the same instance", () => {
-    const currencyApiService = CurrencyApiService.getInstance();
-    const currencyApiService2 = CurrencyApiService.getInstance();
-
-    expect(currencyApiService).toBe(currencyApiService2);
-  });
-
-  test("Destroy an instance", () => {
-    const currencyApiService = CurrencyApiService.getInstance();
-
-    CurrencyApiService.destroyInstance();
-
-    const currencyApiService2 = CurrencyApiService.getInstance();
-
-    expect(currencyApiService).not.toBe(currencyApiService2);
-  });
-
-  test("Get exchange currency ", async () => {
-    const currencyApiService = CurrencyApiService.getInstance();
-
-    const result = await currencyApiService.getExchangeCurrency({
-      amount: 1,
-      from: "",
-      to: "",
+      expect(currencyApiService).toBeInstanceOf(CurrencyApiService);
     });
 
-    expect(result).toStrictEqual({});
+    test("Get twice the same instance", () => {
+      const currencyApiService = CurrencyApiService.getInstance();
+      const currencyApiService2 = CurrencyApiService.getInstance();
+
+      expect(currencyApiService).toBe(currencyApiService2);
+    });
+
+    test("Destroy an instance", () => {
+      const currencyApiService = CurrencyApiService.getInstance();
+
+      CurrencyApiService.destroyInstance();
+
+      const currencyApiService2 = CurrencyApiService.getInstance();
+
+      expect(currencyApiService).not.toBe(currencyApiService2);
+    });
+  });
+
+  describe("Test about methods of class", () => {
+    beforeEach(() => {
+      CurrencyApiService.destroyInstance();
+    });
+    test("Get exchange currency ", () => {
+      const currencyApiService = CurrencyApiService.getInstance();
+
+      expect(
+        currencyApiService.getExchangeCurrency({
+          amount: 1,
+          from: "USD",
+          to: "EUR",
+        })
+      ).resolves.toStrictEqual({});
+    });
+
+    test("Throw an error from API", () => {
+      mockGetFn.mockRejectedValueOnce(new Error("Test error"));
+      const currencyApiService = CurrencyApiService.getInstance();
+
+      expect(
+        currencyApiService.getExchangeCurrency({
+          amount: 1,
+          from: "USD",
+          to: "EUR",
+        })
+      ).rejects.toHaveProperty("message", "Test error");
+    });
   });
 });
