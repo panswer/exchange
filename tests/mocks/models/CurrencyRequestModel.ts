cf6acbf17a8d59d4446f7de26c8892449df10549
ricardo mejias <ricardo@mftech.io>
2022-11-11 14:45:35 -0400

    rename and save request and response
    rename describes and test (it) in CurrencyRequestModel, save request and response in a file too

---
+++ tests/mocks/models/CurrencyRequestModel.ts
@@ -0,0 +1,7 @@
+export const requestDataSuccess = {
+    amount: 1,
+    amountResult: 1,
+    currencyFrom: "USD",
+    currencyTo: "EUR",
+    username: "ricardo@mftech.io",
+  }
--- tests/models/CurrencyRequestModel.test.ts
+++ tests/models/CurrencyRequestModel.test.ts
@@ -2,24 +2,26 @@ import CurrencyRequestModel from "../../src/models/CurrencyRequestModel";
 
 import * as AWSMock from "aws-sdk-mock";
 
+import { requestDataSuccess } from "../mocks/models/CurrencyRequestModel";
+
 const dynamodbDocumentClient = "DynamoDB.DocumentClient";
 
-describe("Currency Request Model", () => {
-  describe("Test about instance of class", () => {
-    test("Get an instance", () => {
+describe("CurrencyRequestModel - models", () => {
+  describe("Should test to get and reset the class", () => {
+    test("Should test to get an instance of CurrencyRequestModel", () => {
       const currencyRequestModel = CurrencyRequestModel.getInstance();
 
       expect(currencyRequestModel).toBeInstanceOf(CurrencyRequestModel);
     });
 
-    test("Get the same instance two times", () => {
+    test("Should test to get the same instance two times", () => {
       const currencyRequestModel1 = CurrencyRequestModel.getInstance();
       const currencyRequestModel2 = CurrencyRequestModel.getInstance();
 
       expect(currencyRequestModel1).toBe(currencyRequestModel2);
     });
 
-    test("Destroy an old instance", () => {
+    test("Should test to get an instance and destroy it", () => {
       const currencyRequest1 = CurrencyRequestModel.getInstance();
 
       CurrencyRequestModel.destroyInstance();
@@ -38,34 +40,27 @@ describe("Currency Request Model", () => {
       AWSMock.restore(dynamodbDocumentClient);
     });
 
-    test("Save a currency request", async () => {
+    test("Should test to save a new currency request", () => {
       AWSMock.mock(dynamodbDocumentClient, "put", (param, callback) => {
         callback(undefined, {});
       });
+
       const currencyRequestModel = CurrencyRequestModel.getInstance();
 
-      const result = await currencyRequestModel.saveCurrencyRequest({
-        amount: 1,
-        amountResult: 1,
-        currencyFrom: "USD",
-        currencyTo: "EUR",
-        username: "ricardo@mftech.io",
-      });
-
-      expect(result).toStrictEqual({});
+      expect(
+        currencyRequestModel.saveCurrencyRequest(requestDataSuccess)
+      ).resolves.toStrictEqual({});
     });
 
-    test("Get all currencies request by username", async () => {
+    test("Should test to get all currencies request by username on session", () => {
       AWSMock.mock(dynamodbDocumentClient, "query", (param, callback) => {
         callback(undefined, {});
       });
       const currencyRequestModel = CurrencyRequestModel.getInstance();
 
-      const result = await currencyRequestModel.getRequestsByUsername(
-        "test@mftech.io"
-      );
-
-      expect(result).toStrictEqual({});
+      expect(
+        currencyRequestModel.getRequestsByUsername("test@mftech.io")
+      ).resolves.toStrictEqual({});
     });
   });
 });
